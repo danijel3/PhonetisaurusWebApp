b7a823e8a6e66dac14a6d6d5ad8ef4dcbf3f99f9
Danijel Koržinek <danijel@pjwstk.edu.pl>
2021-10-29 00:37:15 +0200

    Update settings.py
    Mismatch between config and Dockerfile

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 config = {'data': Path('./data'),
-          'phonetisaurus_bin': Path('/opt/kaldi/tools/phonetisaurus-g2p'),
-          'srilm_bin': Path('/opt/kaldi/tools/srilm/bin/i686-m64')}
+          'phonetisaurus_bin': Path('/usr/local/bin'),
+          'srilm_bin': Path('/usr/local/bin')}
